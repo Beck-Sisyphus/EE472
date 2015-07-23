c9204fe5b9568541be770706e2349eb0427c125d
Beck-Sisyphus <pshfls@gmail.com>
2015-07-22 21:35:01 -0700

    variables all declared and initialized, functions declared, ready for lab 3

---
+++ Lab3/lab3Funct.c
@@ -0,0 +1,365 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include "lab3.h"
+#include "inc/hw_gpio.h"
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/debug.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/sysctl.h"
+#include "drivers/rit128x96x4.h"
+#include "inc/lm3s8962.h"
+#include "utils/ustdlib.h"
+
+// Constants defined in main
+extern const unsigned short MAX_BATT_LEVEL;
+extern const unsigned short HALF_BATT_LEVEL;
+extern const unsigned short BATT_WARN_LEVEL;
+extern const uint32_t MAX_FUEL_LEVEL;
+extern const uint32_t HALF_FUEL_LEVEL;
+extern const uint32_t FUEL_WARN_LEVEL;
+extern const unsigned short TASK_QUEUE_LENGTH;
+extern unsigned short globalCount;
+extern unsigned short blinkTimer;
+extern uint32_t fuelLevellll;
+
+// local variable used in functions
+const int fuelBuringRatio = 20000; // Set as a large number in demo
+
+// Control the major or minor cycle in main function
+void schedule(scheduleDataStruct scheduleData){
+    Bool* isMajorCycle = (Bool*) scheduleData.isMajorCyclePtr;
+
+    if (0  == globalCount) {*isMajorCycle = TRUE;} else {*isMajorCycle = FALSE;}			//Execute a Major Cycle when the count is zero.
+
+    delay_ms(7500);
+    globalCount = (globalCount + 1) % (TASK_QUEUE_LENGTH - 1); //count to 5, then start over again
+    blinkTimer = (blinkTimer + 1) % 8;
+
+}
+
+// Requires: power sub data struct
+// Modifies: powerConsumption, powerGeneration, battLevel, panelState
+void powerSub(void* taskDataPtr){
+    // // Start oscillascope measurement
+    // GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_4, 0xFF);
+
+	powerSubDataStruct* dataPtr = (powerSubDataStruct*) taskDataPtr;
+
+	// unsigned short* globalCount = (unsigned short*) dataPtr->globalCountPtr;
+	// Bool* isMajorCycle = (Bool*) dataPtr->isMajorCyclePtr;
+	unsigned short* battLevel = (unsigned short*) dataPtr->battLevelPtr;
+	unsigned short* powerConsumption = (unsigned short*) dataPtr->powerConsumptionPtr;
+	unsigned short* powerGeneration = (unsigned short*) dataPtr->powerGenerationPtr;
+	Bool* panelState = (Bool*) dataPtr->panelStatePtr;
+
+	//powerConsumption
+	static unsigned short runCount = 1;         //tracks even/odd calls of this function
+	static Bool consumpUpDown = TRUE;
+        
+    //powerGeneration
+    if (!(*panelState)) {                       //else solar panel not deployed...
+        if ((*battLevel)<=30){                  //if battery less than/equal to 10%
+            (*panelState) = TRUE;               //deploy solar panel
+        }
+    } 
+    if (*panelState) {                          //if solar panel is deployed...
+        if ((*battLevel)>95){		            //if battery greater than 75%
+            (*powerGeneration) = 0;             //SPEC CHANGE
+            (*panelState)=FALSE;		        //retract solar panel
+        }
+        else{					                //else battery less than/equal to 95%
+            if (0==runCount){			        //on even calls...
+                (*powerGeneration) += 2;		//increment by 2
+            }
+            else if((*battLevel)<=50){	        //on odd calls... while battery less than/equal to 50%
+                (*powerGeneration) += 1;		//increment by 1
+            }
+        }
+    } 
+
+    runCount = !runCount;			            //alternates between zero and non zero for odd/een calls respectively
+    if (consumpUpDown) {
+        if (0==runCount){				        //on even calls...
+            (*powerConsumption) += 2;		    //increment by 2
+            if ((*powerConsumption)>=10) {
+                consumpUpDown = FALSE;
+            }
+        } 
+        else{						            //on odd calls...
+            (*powerConsumption) -= 1;		    //decrement by 1
+        }
+    }
+    else {
+        if (0==runCount){		                //on even calls...
+            (*powerConsumption) -= 2;	        //decrement by 2
+            if ((*powerConsumption)<=5) {
+                consumpUpDown = TRUE;
+            }
+        }
+        else{						            //on odd calls...
+            (*powerConsumption) += 1;		    //increment by 1
+        }
+    }
+            
+        
+	//batteryLevel
+	if (!(*panelState)){
+		(*battLevel) = (*battLevel) - 3*(*powerConsumption);
+	}
+	else{
+		(*battLevel) = (*battLevel) - (*powerConsumption) + (*powerGeneration);
+	}
+	
+        if(((*battLevel)>100)&((*battLevel)<300)){ //"OVERLOAD PROTECTION"
+		(*battLevel)=100;
+	}
+	if((*battLevel)>65000){                        //"NEGATIVE PROTECTION"
+		(*battLevel)=0;
+	}
+
+    // // End oscillascope measurement
+    // GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_4, 0x00);
+}
+
+void solarPanelControl(void* taskDataPtr) {
+
+}
+
+// Communication only store the command without decoding it
+// Require : satellite communication data struct,
+//           randomInteger function;
+// Modifies: thrust command.
+void satelliteComms(void* taskDataPtr){
+        
+    satelliteCommsDataStruct* commPtr = (satelliteCommsDataStruct*) taskDataPtr;
+
+    unsigned short* globalCount = (unsigned short*) commPtr->globalCountPtr;
+    Bool* isMajorCycle = (Bool*) commPtr->isMajorCyclePtr;
+    if (isMajorCycle)
+    {
+        // send info, use print as a method to send
+        Bool* fuelLowSignal = (Bool*)commPtr->fuelLowPtr;
+        Bool* battLowSignal = (Bool*)commPtr->battLowPtr;
+        unsigned short* battLevelSignal = (unsigned short*)commPtr->battLevelPtr;
+        uint32_t* fuelLevelSignal = (uint32_t*)commPtr->fuelLevelPtr;
+        unsigned short* powerConsumptionSignal = (unsigned short*)commPtr->powerConsumptionPtr;
+        unsigned short* powerGenerationSignal = (unsigned short*)commPtr->powerGenerationPtr;
+        Bool* panelStateSignal = (Bool*)commPtr->panelStatePtr; 
+        //printf("fuel low is : %u\n", *fuelLowSignal);
+        //printf("battery low is : %u\n", *battLowSignal);
+        //printf("battery level : %hu\n", *battLevelSignal);
+        //printf("fuel level : %u\n", *fuelLevelSignal);
+        //printf("power consumption is: %hu\n", *powerConsumptionSignal);
+        //printf("power generation is : %hu\n", *powerGenerationSignal);
+        //printf("panel state is : %u\n", *panelStateSignal);
+
+        // receive (rando) thrust commands, generate from 0 to 2^16 -1
+        // uint16_t thrustCommand = randomInteger(globalCount);
+        uint16_t thrustCommand = 0x0FF1;
+        *(uint16_t*)(commPtr->thrustPtr) = thrustCommand;
+    }
+    return;
+}
+
+void vehicleComms(void* taskDataPtr){
+
+}
+
+// Require : the minor clock running at 1 second per cycle, 
+//			and thruster sub data struct; 
+// Modifies: global constant fuelLevel, taking in count of duration.
+void thrusterSub(void* taskDataPtr){
+
+	thrusterSubDataStruct* thrustCommandPtr = (thrusterSubDataStruct*) taskDataPtr;
+        
+	// unsigned short* globalCount = (unsigned short*) thrustCommandPtr->globalCountPtr;
+	Bool* isMajorCycle = (Bool*) thrustCommandPtr->isMajorCyclePtr;
+	uint32_t* fuelPtr = (uint32_t*) thrustCommandPtr->fuelLevelPtr;
+
+	unsigned short left = 0;
+	unsigned short right = 0;
+	unsigned short up = 0;
+	unsigned short down = 0;
+	static unsigned short magnitude = 0;
+	static unsigned short duration = 0;
+
+	// Only updates the magnitude when the duration is positive, otherwise no change on magnitude
+	if (*isMajorCycle)
+	{
+		uint16_t command = *(uint16_t*)(thrustCommandPtr->thrustPtr);
+
+		if (command & 0x0001) { left  = 1; } else { left  = 0;}
+		if (command & 0x0002) { right = 1; } else { right = 0;}
+		if (command & 0x0004) { up    = 1; } else { up    = 0;}
+		if (command & 0x0008) { down  = 1; } else { down  = 0;}
+		// get duration, unsigned char pointer moves every 8 bits
+		// so it can separate first 8 bits and last 8 bits
+		duration = *((unsigned char*)&command + 1);
+		if (duration) { magnitude = (command >> 4) & 0x000F; }
+	}
+
+	// If the new command ask use a 
+	if (duration)
+	{
+		*fuelPtr -= magnitude * fuelBuringRatio;
+		duration -= 1;
+	}
+
+	// When the fuel level goes below 0, the unsigned int when to really big
+	if (*fuelPtr > MAX_FUEL_LEVEL)
+	{
+		*fuelPtr = 0;
+	}
+        
+    // Convert inner counter to 100 scale for OLED display
+    fuelLevellll = ((*fuelPtr) * 100) / MAX_FUEL_LEVEL;
+    // Memory protection, since we detect memory corruption at fuelLevellll
+    int *temp = (int *)malloc(10*sizeof(int));
+    free(temp);
+}
+
+//TODO
+void oledDisplay(void* taskDataPtr){
+    
+  
+    oledDisplayDataStruct* dataPtr = (oledDisplayDataStruct*) taskDataPtr;
+    
+    unsigned short* battLevel = (unsigned short*) dataPtr->battLevelPtr;
+    uint32_t* fuelLevelPtr2 = (uint32_t*) dataPtr->fuelLevelPtr;
+    unsigned short* powerConsumption = (unsigned short*) dataPtr->powerConsumptionPtr;
+    Bool* panelState = (Bool*) dataPtr->panelStatePtr;
+    Bool* fuelLow = (Bool*) dataPtr->fuelLowPtr;
+    Bool* battLow = (Bool*) dataPtr->battLowPtr;
+
+    unsigned short* globalCount = (unsigned short*) dataPtr->globalCountPtr;
+    Bool* isMajorCycle = (Bool*) dataPtr->isMajorCyclePtr;
+
+    // Push select button to change to annunciation mode on OLED
+    long buttonRead = 2;
+    char* bufferPtr;
+
+    // Pushed = 0, released = 2 from our measurement
+    buttonRead = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_1);
+ 
+    if (2 == buttonRead)    // Status display mode
+    {
+        RIT128x96x4Clear();
+
+        // Display panel state.
+        char panelDepl = (1 == *panelState) ? 'Y' : 'N';
+        RIT128x96x4StringDraw("Panel Deployed: ", 5, 10, 15);
+        RIT128x96x4StringDraw(&panelDepl, 5, 20, 25);
+        
+        // Display battery level. Cast interger to char array using snprintf
+        snprintf(bufferPtr, 20, "%d", *battLevel);
+        RIT128x96x4StringDraw("Battery Level: ", 5, 30, 15);
+        RIT128x96x4StringDraw( bufferPtr , 5, 40, 25);
+
+        // Display fuel level.
+        snprintf(bufferPtr, 20, "%d", fuelLevellll);
+        RIT128x96x4StringDraw("Fuel Level: ", 5, 50, 15);
+        RIT128x96x4StringDraw( bufferPtr , 5, 60, 25);
+
+        // Display power consumption.
+        snprintf(bufferPtr, 20, "%d", *powerConsumption);
+        RIT128x96x4StringDraw("Power Consumption: ", 5, 70, 15);
+        RIT128x96x4StringDraw( bufferPtr , 5, 80, 25);
+      
+    } else if (0 == buttonRead) // Annunciation mode
+    {
+        RIT128x96x4Clear();
+        
+        // Display fuel low flag.
+        char fuelLowStr = (1 == *fuelLow) ? 'Y' : 'N';
+        RIT128x96x4StringDraw("Fuel Low: ", 5, 10, 15);
+        RIT128x96x4StringDraw(&fuelLowStr, 5, 20, 25);
+
+        // Display battery low flag.
+        char battLowStr = (1 == *battLow) ? 'Y' : 'N';
+        RIT128x96x4StringDraw("Battery Low: ", 5, 30, 15);
+        RIT128x96x4StringDraw( &battLowStr , 5, 40, 25);
+    }
+
+    return;
+}
+
+void consoleKeyboard(void* taskDataPtr){
+
+}
+
+// Require : warning alarm data struct;
+// Modifies: fuelLow pointer, battLow pointer.
+void warningAlarm(void* taskDataPtr){
+	
+    warningAlarmDataStruct* dataPtr = (warningAlarmDataStruct*) taskDataPtr;
+    Bool* fuelLow = (Bool*)dataPtr->fuelLowPtr;
+    Bool* battLow = (Bool*)dataPtr->battLowPtr;
+    unsigned short* battLevel = (unsigned short*)dataPtr->battLevelPtr;
+    uint32_t* fuelLevel = (uint32_t*)dataPtr->fuelLevelPtr;
+    unsigned short* globalCount = (unsigned short*) dataPtr->globalCountPtr;
+    Bool* isMajorCycle = (Bool*) dataPtr->isMajorCyclePtr;
+
+    if (*fuelLevel < FUEL_WARN_LEVEL) { *fuelLow = TRUE; } else { *fuelLow = FALSE; }
+    if (*battLevel < BATT_WARN_LEVEL) { *battLow = TRUE; } else { *battLow = FALSE; }
+    
+    if (((*battLevel)>HALF_BATT_LEVEL)&((*fuelLevel)>HALF_FUEL_LEVEL)){
+        //display solid green LED
+        GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_5, 0xFF);
+        //clear yellow LED
+        GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_6, 0x00);
+        //clear red LED
+        GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_7, 0x00);
+    }
+    else{
+        //clear solid green LED
+        GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_5, 0x00);
+        //SPEC CHANGE!!! YELLOW is BATT, RED is FUEL
+            if ((*battLevel<BATT_WARN_LEVEL)&(*battLevel<HALF_BATT_LEVEL)){
+                //TODO flash yellow 1 sec
+                if ((0==blinkTimer)|(2==blinkTimer)|(4==blinkTimer)|
+                    (6==blinkTimer))
+                    GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_6, 0xFF);
+                if ((1==blinkTimer)|(3==blinkTimer)|(5==blinkTimer)|
+                    (7==blinkTimer))
+                    GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_6, 0x00);
+            }
+            else if (*battLevel<HALF_BATT_LEVEL){
+                //TODO flash yellow 2 sec
+                if ((0==blinkTimer)|(1==blinkTimer)|(4==blinkTimer)|(5==blinkTimer))  
+                    GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_6, 0xFF);
+                if ((2==blinkTimer)|(3==blinkTimer)|(6==blinkTimer)|(7==blinkTimer))  
+                    GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_6, 0x00);
+            }
+            if ((*fuelLevel<FUEL_WARN_LEVEL)&(*fuelLevel<HALF_FUEL_LEVEL)){
+                //TODO flash red 1 sec
+                if ((0==blinkTimer)|(2==blinkTimer)|(4==blinkTimer)|(6==blinkTimer))  
+                    GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_7, 0xFF);
+                if ((1==blinkTimer)|(3==blinkTimer)|(5==blinkTimer)|(7==blinkTimer))  
+                    GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_7, 0x00);
+            }
+            else if (*fuelLevel<HALF_FUEL_LEVEL){
+                //TODO flash red 2 sec
+                if ((0==blinkTimer)|(1==blinkTimer)|(4==blinkTimer)|(5==blinkTimer))  
+                    GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_7, 0xFF);
+                if ((2==blinkTimer)|(3==blinkTimer)|(6==blinkTimer)|(7==blinkTimer))  
+                    GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_7, 0x00);
+            }
+     }
+     
+     return;
+}
+
+void delay_ms(int time_in_ms){
+	volatile unsigned long i = 0;
+    volatile unsigned int j = 0;
+    
+    for (i = time_in_ms; i > 0; i--)
+    {
+        for (j = 0; j < 100; j++);
+    }
+    return;
+}
---
+++ Lab3/lab3Main.c
@@ -0,0 +1,195 @@
+// Include Statements
+#include <stdlib.h>
+#include <stdint.h>
+#include "lab3.h"
+#include "inc/hw_gpio.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+#include "drivers/rit128x96x4.h"
+
+// Define some Constants
+const unsigned short MAX_BATT_LEVEL = 100;
+const unsigned short HALF_BATT_LEVEL = 50;
+const unsigned short BATT_WARN_LEVEL = 10;
+// 6 month has x = 6*30*24*60*60 seconds, and 100% has y = x/20 seconds, 
+// since the magnitude command has 4 bit, aka the consumption ranks from 0 to 15, 
+// setting a total fuel level as 15 * y helps to calculate the fuel easier
+// Assuming the minor cycle is running at 1 second per cycle rate.
+const uint32_t MAX_FUEL_LEVEL = 11664000;
+const uint32_t HALF_FUEL_LEVEL = 5832000; // at 50% level
+const uint32_t FUEL_WARN_LEVEL = 1166400; // below 10% is warnning level
+const unsigned short TASK_QUEUE_LENGTH = 6;
+
+// Define Global Variables storing status data
+unsigned int* battLevelPtr;
+uint32_t fuelLevel;
+unsigned short powerConsumption;
+unsigned short powerGeneration;
+Bool panelState;
+Bool panelDeploy;
+Bool panelRetract;
+Bool panelMotorSpeedUp;
+Bool panelMotorSpeedDown;
+// 16bit encoded thrust command [15:8]Duration,[7:4]Magnitude,[3:0]Direction
+uint16_t thrust; 
+Bool fuelLow;
+Bool battLow;
+Bool isMajorCycle;
+
+// Added for lab 3
+char vehicleCommand;
+char vehicleResponse;
+
+// Global variable created for passing data through different function safer
+unsigned short globalCount;
+unsigned short blinkTimer;
+uint32_t fuelLevellll;
+
+int main(){
+	enableOLED();
+
+	// Initialization 
+	unsigned int battLevel[16];
+	battLevelPtr = battLevel; // equivalent to = &battLevel[0]
+
+	fuelLevel = 0;
+	powerConsumption = 0;
+	powerGeneration = 0;
+	panelState = FALSE;
+	panelDeploy = FALSE;
+	panelRetract = FALSE;
+	panelMotorSpeedUp = FALSE;
+	panelMotorSpeedDown = FALSE;
+
+	thrust = 0;
+	fuelLow = FALSE;
+	battLow = FALSE;
+
+	// vehicleCommand ＝ NULL; Can't initialize as NULL, just left it as its default
+	// vehicleResponse = NULL;
+
+	isMajorCycle = TRUE;
+	globalCount = 0;
+	blinkTimer = 0;
+
+	// Define Data Structs
+	powerSubDataStruct powerSubData           = {&panelState, &panelDeploy, &panelRetract, &battLevel, &powerConsumption, &powerGeneration};
+	solarPanelStruct solarPanelData           = {&panelState, &panelDeploy, &panelRetract, &panelMotorSpeedUp, &panelMotorSpeedDown, &globalCount, &isMajorCycle};
+	satelliteCommsDataStruct satelliteCommsData     = {&fuelLow, &battLow, &panelState, &battLevel, &fuelLevel, &powerConsumption, &powerGeneration, &thrust, &globalCount, &isMajorCycle};
+	thrusterSubDataStruct thrusterSubData     = {&thrust, &fuelLevel, &globalCount, &isMajorCycle};
+	vehicleCommsStruct vehicleCommsData         = {&vehicleCommand, &vehicleResponse, &globalCount, &isMajorCycle};
+	oledDisplayDataStruct oledDisplayData     = {&fuelLow, &battLow, &panelState, &battLevel, &fuelLevel, &powerConsumption, &powerGeneration, &globalCount, &isMajorCycle};
+	keyboardDataStruct keyboardData           = {&panelMotorSpeedUp, &panelMotorSpeedDown};
+	warningAlarmDataStruct warningAlarmData   = {&fuelLow, &battLow, &battLevel, &fuelLevel, &globalCount, &isMajorCycle};
+	scheduleDataStruct scheduleData           = {&globalCount, &isMajorCycle};
+
+	// Define TCBs
+	TCB powerSubTCB;
+	TCB solarPanelTCB;
+	TCB satelliteCommsTCB;
+	TCB thrusterSubTCB;
+	TCB vehicleCommsTCB;
+	TCB oledDisplayTCB;
+	TCB keyboardDataTCB;
+	TCB warningAlarmTCB;
+	TCB* TCBptr; //ptr to active TCB
+
+	// Populate TCBs
+	powerSubTCB.taskDataPtr = (void*)&powerSubData;
+	powerSubTCB.taskPtr = powerSub;
+
+	solarPanelTCB.taskDataPtr = (void*)&solarPanelData;
+	solarPanelTCB.taskPtr = solarPanelControl;
+
+	satelliteCommsTCB.taskDataPtr = (void*)&satelliteCommsData;
+	satelliteCommsTCB.taskPtr = satelliteComms;
+
+	thrusterSubTCB.taskDataPtr = (void*)&thrusterSubData;
+	thrusterSubTCB.taskPtr = thrusterSub;
+
+	vehicleCommsTCB.taskDataPtr = (void*)&vehicleCommsData;
+	vehicleCommsTCB.taskPtr = vehicleComms;
+
+	oledDisplayTCB.taskDataPtr = (void*)&oledDisplayData;
+	oledDisplayTCB.taskPtr = oledDisplay;
+
+	keyboardDataTCB.taskDataPtr = (void*)&keyboardData;
+	keyboardDataTCB.taskPtr = consoleKeyboard;
+
+	warningAlarmTCB.taskDataPtr = (void*)&warningAlarmData;
+	warningAlarmTCB.taskPtr = warningAlarm;
+
+
+	// Task Queue 
+	TCB* taskQueue[8];
+
+	taskQueue[0] = &powerSubTCB;
+	taskQueue[1] = &solarPanelTCB;
+	taskQueue[2] = &satelliteCommsTCB;
+	taskQueue[3] = &thrusterSubTCB;
+	taskQueue[4] = &vehicleCommsTCB;
+	taskQueue[5] = &oledDisplayTCB;
+	taskQueue[6] = &keyboardDataTCB;
+	taskQueue[7] = &warningAlarmTCB;
+
+	enableGPIO();
+
+    // Run... forever!!!
+    while(1){
+    	// dispatch each task in turn
+    	// can not use a for loop nested in while loop for cross compile
+        TCBptr = taskQueue[0];
+        TCBptr->taskPtr( (TCBptr->taskDataPtr) );
+        TCBptr = taskQueue[1];
+        TCBptr->taskPtr( (TCBptr->taskDataPtr) );
+        TCBptr = taskQueue[2];
+        TCBptr->taskPtr( (TCBptr->taskDataPtr) );
+        TCBptr = taskQueue[3];
+        TCBptr->taskPtr( (TCBptr->taskDataPtr) );
+        TCBptr = taskQueue[4];
+        TCBptr->taskPtr( (TCBptr->taskDataPtr) );
+        TCBptr = taskQueue[5];
+        TCBptr->taskPtr( (TCBptr->taskDataPtr) );
+        TCBptr = taskQueue[6];
+        TCBptr->taskPtr( (TCBptr->taskDataPtr) );
+        TCBptr = taskQueue[7];
+        TCBptr->taskPtr( (TCBptr->taskDataPtr) );
+    	schedule(scheduleData);
+    }
+    return EXIT_SUCCESS;
+}
+
+void enableOLED() {
+	// Initialize SysClk.
+	SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
+		SYSCTL_XTAL_8MHZ);
+        
+	// Initialize the OLED display.
+	RIT128x96x4Init(1000000);
+}
+
+void enableGPIO() {
+	// Enable GPIO C
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
+	// Set pins C4, C5, C6, C7 as an output
+	GPIOPinTypeGPIOOutput(GPIO_PORTC_BASE, GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|
+	                      GPIO_PIN_7);
+
+	// Set select button as input with a pull up resistor 
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+
+	GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_1);
+	    
+	GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_1, GPIO_STRENGTH_2MA,
+	                     GPIO_PIN_TYPE_STD_WPU);
+
+			
+	// clear green LED		
+	GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_5, 0x00);		
+	// clear yellow LED		
+	GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_6, 0x00);		
+	// clear red LED		
+	GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_7, 0x00);
+}
